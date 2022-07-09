bb415aa518fba8a5365292096c9b20512066c56d
yeosiowvic <victoryeo1974@gmail.com>
2022-07-09 12:43:14 +0800

    add new type of CreateBookInput

--- models/book.go
+++ models/book.go
@@ -5,3 +5,8 @@ type Book struct {
 	Title  string `json:"title"`
 	Author string `json:"author"`
 }
+
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
