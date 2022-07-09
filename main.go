71108a73373b22715bcce3bece910cbc145b581a
yeosiowvic <victoryeo1974@gmail.com>
2022-07-09 12:12:34 +0800

    add models file

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
 	"errors"
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/victoryeo/golang-restapi/models"
 )
 
 type todo struct {
-	ID string `json:"id"`
-	Item string `json:"title"`
-	Completed bool `json:"completed"`
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
 var todos = []todo{
-  {ID:"1", Item:"Clean room", Completed: false},
-  {ID:"2", Item:"Record room", Completed: false},
-  {ID:"3", Item:"Read room", Completed: false},
+	{ID: "1", Item: "Clean room", Completed: false},
+	{ID: "2", Item: "Record room", Completed: false},
+	{ID: "3", Item: "Read room", Completed: false},
 }
 
 func getRoot(context *gin.Context) {
-  fmt.Print("getRoot")
+	fmt.Print("getRoot")
 	// Call the HTML method of the Context to render a template
 	context.HTML(
 		// Set the HTTP status to 200 (OK)
@@ -29,43 +31,43 @@ func getRoot(context *gin.Context) {
 		"index.html",
 		// Pass the data that the page uses (in this case, 'title')
 		gin.H{
-				"title": "Home Page",
+			"title": "Home Page",
 		},
-  )
+	)
 }
 
 func getTodos(context *gin.Context) {
-  context.IndentedJSON(http.StatusOK, todos)
+	context.IndentedJSON(http.StatusOK, todos)
 }
 
 func getTodoByID(id string) (*todo, error) {
 
 	for i, t := range todos {
-			if t.ID == id {
-					return &todos[i], nil
-			}
+		if t.ID == id {
+			return &todos[i], nil
+		}
 	}
-  return nil, errors.New("todo not found")
+	return nil, errors.New("todo not found")
 }
 
 func getTodo(context *gin.Context) {
 	id := context.Param("id")
 	todo, err := getTodoByID(id)
 	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message":"not found"})
-	  return
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+		return
 	}
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
 func addTodo(context *gin.Context) {
-  var newTodo todo
-  if err := context.BindJSON(&newTodo); err!= nil {
-    return
-  }
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
 
-  todos = append(todos, newTodo)
-  context.IndentedJSON(http.StatusCreated, newTodo)
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
 func toggleTodoStatus(context *gin.Context) {
@@ -73,26 +75,28 @@ func toggleTodoStatus(context *gin.Context) {
 	todo, err := getTodoByID(id)
 
 	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message":"not found"})
-	  return
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+		return
 	}
 
 	todo.Completed = !todo.Completed
 	context.IndentedJSON(http.StatusOK, todo)
 }
- 
+
 func main() {
-  fmt.Print("Code is a ", " portal.\n")
-  router := gin.Default()
+	fmt.Print("Code is a ", " portal.\n")
+	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-  router.GET("/", getRoot)
-  router.GET("/todos", getTodos)
+	models.ConnectDatabase()
+
+	router.GET("/", getRoot)
+	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
 	router.PATCH("/todos/:id", toggleTodoStatus)
-  router.POST("/todos", addTodo)
-  router.NoRoute(func(c *gin.Context) {
-    c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
-  })
-  router.Run("localhost:9090")
+	router.POST("/todos", addTodo)
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+	router.Run("localhost:9090")
 }
