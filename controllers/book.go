dcb239b60f9fe98e3c9477e6c14c24d01fc10a07
yeosiowvic <victoryeo1974@gmail.com>
2022-07-09 12:42:56 +0800

    add controllers for book models

---
+++ controllers/book.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/victoryeo/golang-restapi/models"
+)
+
+// GET /books
+// Get all books
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+// POST /books
+// Create new book
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input models.CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
