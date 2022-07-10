5acbc52bfb4b9abc0787f74d6d873a27b54c7d41
yeosiowvic <victoryeo1974@gmail.com>
2022-07-10 10:44:48 +0800

    add jwt middleware code

---
+++ middleware/JWTmiddleware.go
@@ -0,0 +1,103 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/victoryeo/golang-restapi/models"
+)
+
+func GetSecretKey() string {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = "secret"
+	}
+	return secret
+}
+
+func ValidateToken(encodedToken string) (*jwt.Token, error) {
+	return jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
+			// %q which is used for a single-quoted character
+			return nil, fmt.Errorf("Invalid token %q", token.Header["alg"])
+
+		}
+		return []byte(GetSecretKey()), nil
+	})
+}
+
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const BEARER_SCHEMA = "Bearer"
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		token, err := ValidateToken(tokenString)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
+
+func extractBearerToken(header string) (string, error) {
+	if header == "" {
+		return "", errors.New("No header value given")
+	}
+
+	jwtToken := strings.Split(header, " ")
+	if len(jwtToken) != 2 {
+		return "", errors.New("incorrectly formatted authorization header")
+	}
+
+	return jwtToken[1], nil
+}
+
+func parseToken(jwtToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		if _, OK := token.Method.(*jwt.SigningMethodHMAC); !OK {
+			return nil, errors.New("bad signed method received")
+		}
+		return []byte(GetSecretKey()), nil
+	})
+
+	if err != nil {
+		return nil, errors.New("bad jwt token")
+	}
+
+	return token, nil
+}
+
+func JWTTokenCheck(c *gin.Context) {
+	jwtToken, err := extractBearerToken(c.GetHeader("Authorization"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.UnsignedResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	token, err := parseToken(jwtToken)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.UnsignedResponse{
+			Message: "bad jwt token",
+		})
+		return
+	}
+
+	_, OK := token.Claims.(jwt.MapClaims)
+	if !OK {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, models.UnsignedResponse{
+			Message: "unable to parse claims",
+		})
+		return
+	}
+	c.Next()
+}
