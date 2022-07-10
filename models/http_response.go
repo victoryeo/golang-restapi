94478f4b460ce30128b27ce68504cf21088a1d77
yeosiowvic <victoryeo1974@gmail.com>
2022-07-10 10:45:12 +0800

    add Responses type

---
+++ models/http_response.go
@@ -0,0 +1,10 @@
+package models
+
+type UnsignedResponse struct {
+	Message interface{} `json:"message"`
+}
+
+type SignedResponse struct {
+	Token   string `json:"token"`
+	Message string `json:"message"`
+}
