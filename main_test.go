10c74c0f9d97446760f65d60c35685a8129fcd98
yeosiowvic <victoryeo1974@gmail.com>
2022-07-10 22:30:01 +0800

    update test file

--- main_test.go
+++ main_test.go
@@ -3,11 +3,12 @@
 package main
 
 import (
-	"net/http"
-  "net/http/httptest"
-  "testing"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"strings"
+	"testing"
 )
 
 // Test that a GET request to the home page returns the home page with
@@ -15,18 +16,20 @@ import (
 func TestShowIndexPageUnauthenticated(t *testing.T) {
 	r := getRouter(true)
 
-  r.GET("/", getRoot)
+	r.GET("/", getRoot)
 
-  // Create a request to send to the above route
-  req, _ := http.NewRequest("GET", "/", nil)
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/", nil)
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
-    // Test that the http status code is 200
-    statusOK := w.Code == http.StatusOK
-	
+		// Test that the http status code is 200
+		statusOK := w.Code == http.StatusOK
+
 		// Test that the page title is "Home Page"
-    p, err := ioutil.ReadAll(w.Body)
-		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
+		p, err := ioutil.ReadAll(w.Body)
+		strlen := strings.Index(string(p), "<title>Home Page</title>")
+		fmt.Println("strlen ", strlen)
+		pageOK := err == nil && strlen > 0
 
 		return statusOK && pageOK
 	})
