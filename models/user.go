a10840201a83dd5b49fc940ea8a6c7f7361ed763
yeosiowvic <victoryeo1974@gmail.com>
2022-07-30 07:32:43 +0800

    add user model

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Username string `json:"username" gorm:"unique"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
+}
